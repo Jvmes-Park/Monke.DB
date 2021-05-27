79bfcc243fffa2ea5268204768a4092214712976
James Park <jamesdparkny@gmail.com>
2021-05-27 12:12:36 -0400

    b-tree

--- backend/B-tree.hpp
+++ backend/B-tree.hpp
@@ -7,6 +7,8 @@ class BTreeNode {
 
 	public:
 	BTreeNode(int _t, bool _leaf);
+	void insertNonFull(int k);
+	void splitChild(int i, BTreeNode *y);
 	void traverse();
 	BTreeNode *search(int k);
 	friend class BTree;
@@ -28,6 +30,7 @@ class BTree {
 	BTreeNode* search(int k) {
 		return (root == NULL) ? NULL : root -> search(k);
 	}
+	void insert(int k);
 };
 
 BtreeNode::BTreeNode(int _t, bool _leaf) {
@@ -57,7 +60,71 @@ BTreeNode *BTreeNode::search(int k) {
 	if (leaf == true)
 		return NULL;
 	return C[i] -> search(k);
-}	
+}
+
+void BTree::insert(int k) {
+	if (root == NULL) {
+		root = new BTreeNode(t, true);
+		root -> kets[0] = k;
+		root -> n = 1;
+	}
+	else {
+		if (root -> n == 2*t - 1) {
+			BTreeNode *s = new BTreeNode(t, false);
+			s -> c[0] = root;
+			s -> splitChild(0, root);
+			int i = 0;
+			if (s -> keys[0] < k)
+				i++
+			s -> C[i] -> insertNonFull(k);
+			root = s;
+		}
+		else {
+			root -> insertNonFull(k);
+		}
+	}
+}
+
+void BTreeNode::insertNonFull(int k) {
+	int i = n - 1;
+	if (leaf == true) {
+		while (i >= 0 && kets[i] > k) {
+			keys[i + 1] = keys[i];
+			i--;
+		}
+		keys[i + 1] = k;
+		n = n + 1;
+	}
+	else {
+		while (i >= 0 && keys[i] > k)
+				i--;
+		if (C[i + 1] -> n == 2*t-1) {
+			splitChild(i + 1, C[i + 1]);
+			if (keys[i + 1] < k)
+				i++;
+		}
+		C[i + 1] -> insertNonFull(k);
+	}
+}
+
+void BTreeNode::splitChild(int i, BTreeNode *y) {
+	BTreeNode *z = new BTreenNode(y -> t, y -> leaf);
+	z -> n = t - 1;
+	for (int j = 0; i < t - 1; j++) 
+		z -> keys[j] = y -> keys[j+t];
+	if (y -> leaf == false) {
+		for (int j = 0; j < t; j++)
+			z -> C[j] = y -> C[j+t];
+	}
+	y -> n = t - 1;
+	for (int j = n; j >= i + 1; j--)
+		C[j+1] = C[j];
+	C[i+1] = z;
+	for (int j = n - 1; j >= i; j--)
+		keys[j + 1] = keys[j];
+	keys[i] = y -> leys[t - 1];
+	n = n + 1;
+}
 
 
 
